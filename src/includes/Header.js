79ee7da10ffec39832bffcd545f1659cd3c13625
unknown <ju.velling@gmail.com>
2023-11-23 01:45:36 +0100

    added navigation (fully responsive)

--- src/includes/Header.js
+++ src/includes/Header.js
@@ -1,12 +1,34 @@
-function Header(props) {
-  return (
-    <header>
-      <div className="menu">
-        &#x2261;
-      </div>
-      TimeOut
-    </header>
-  );
+import React from 'react';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { enabled: "false" };
+    this.toggleNav = this.toggleNav.bind(this);
+  }
+
+  render() {
+    const { color } = this.props;
+    return (
+      <header className={color}>
+        <div onClick={this.toggleNav} className="menu">
+          &#x2261;
+        </div>
+        TimeOut
+      </header>
+    );
+  }
+
+  toggleNav() {
+    const nav = document.getElementById("nav");
+    if (this.state.enabled === "false") {
+      nav.setAttribute("enabled", "true");
+      this.setState({ enabled: "true"});
+    } else {
+      nav.setAttribute("enabled", "false");
+      this.setState({ enabled: "false"});
+    }
+  }
 }
 
 export default Header;
